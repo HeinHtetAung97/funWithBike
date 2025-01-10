bd6792d5b9fed2461c29c73d0906973d2ea8a0cc
Hein Htet Aung <warriorvictor97@gmail.com>
2025-01-10 21:26:05 +0630

    about article and login

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Article::factory()->count(10)->create();
+
+        $list = ['Cafe', 'Tracker', 'Scrambler', 'Bobber', 'Brat'];
+        foreach ($list as $type) {
+            Category::create(["name" => $type]);
+        }
+
+
+        User::factory()->create([
+            'name' => 'Hein',
+            'email' => 'hein@gmail.com'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Honey',
+            'email' => 'honey@gmail.com'
+        ]);
+    }
+}
